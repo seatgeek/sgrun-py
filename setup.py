2832e18da2f389ded5f09fdbea78fd5c48ebdbce
Zach Hammer <zhammer@seatgeek.com>
2020-05-21 14:47:22 -0400

    add install_requires ddtrace 0.37.1
    also black

--- setup.py
+++ setup.py
@@ -1,15 +1,12 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='sgrun',
-    version='0.0.1',
+    name="sgrun",
+    version="0.0.1",
     packages=find_packages(),
-    long_description=open('README.md').read(),
-    entry_points={
-        "console_scripts": [
-            "sgrun = sgrun.bootstrap.commandline:main"
-        ]
-    },
+    long_description=open("README.md").read(),
+    install_requires=["ddtrace==0.37.1",],
+    entry_points={"console_scripts": ["sgrun = sgrun.bootstrap.commandline:main"]},
     classifiers=[
         "Programming Language :: Python",
         "Programming Language :: Python :: 2.7",
